44e869a5d7af26289d582f66fe825fd4646a8634
duyvo112 <duyb2111833@student.ctu.edu.vn>
2024-11-04 19:32:14 +0700

    commit app

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,16 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import Vue3Toastify from 'vue3-toastify'
+import 'vue3-toastify/dist/index.css'
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(router)
+  .use(Vue3Toastify, {
+    autoClose: 700,
+    position: 'top-right',
+    clearOnUrlChange: false,
+  })
+  .mount('#app')

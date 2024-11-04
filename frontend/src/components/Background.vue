ae8f8ab53b80f7455279efd53d56ffbc3a51fcc5
duyvo112 <duyb2111833@student.ctu.edu.vn>
2024-11-04 19:30:07 +0700

    Background web

---
+++ frontend/src/components/Background.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="bg-img">
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    name: "BackgroundComponent",
+}
+</script>
+<style scoped>
+.bg-img {
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    background-image: url(../assets/img/pexels-photo-694740.jpeg);
+
+}
+
+.bg-img::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 1;
+}
+
+.bg-img>* {
+    position: relative;
+    z-index: 2;
+}
+</style>
